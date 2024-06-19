1c1e9c1722c9aebe08b4bdbc32f481a4435b199f
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2024-06-19 11:08:02 +1000

    Add files via upload

--- tron3.py
+++ tron3.py
@@ -6,8 +6,6 @@ import time
 mainwin = Tk(className=" TRON")
 
 mainwin.geometry("800x680")
-player1colour = "#A0A0FF"
-player2colour = "#FFA0A0"
 AIcolour = "#0000FF"
 
 # playground
@@ -25,48 +23,36 @@ font1 = ("Arial",16,"bold")
 fontBIG = ("Arial",64,"bold") 
 def printscr(mytext,x,y,mycolour):
     canvastext.create_text(x,y,text=mytext, fill=mycolour,font=font1, anchor="sw") 
-
 def printBIG(mytext,x,y,mycolour):
     canvas1.create_text(x,y,text=mytext, fill=mycolour,font=fontBIG, anchor="sw") 
 
 
 class Player:
-    def __init__(self,x=100,y=100,dx=0,dy=0,alive=True,score=0):
+    def __init__(self,x=100,y=100,dx=0,dy=0,alive=True,score=0, colour="white"):
         self.x = x
         self.y = y
         self.dx = dx
         self.dy = dy
         self.alive = alive
         self.score = score
+        self.colour = colour
     def move(self):
         if self.alive:
            self.x = self.x + self.dx
            self.y = self.y + self.dy
-           drawdot(self.x,self.y,"red")
            if grid[self.x][self.y] == 1:
-            explosion(self.x,self.y)
-            self.alive = False
+               explosion(self.x,self.y)
+               self.alive = False
+           else:
+               drawdot(self.x,self.y,self.colour)
     
 
 
-player1 = Player(75,50,0,1,True,0)
-player2 = Player(150,50,0,1,True,0)
+player1 = Player(75,50,0,1,True,0,"#A0A0FF")
+player2 = Player(160,50,0,1,True,0,"#FFA0A0")
 
 playerai = Player(100,50,0,1,True,0)
 
-player1alive = True
-x1 = 50 # player 1 x-location
-y1 = 50 # player 1 y-location 
-dx1 = 0 # player 1 x-speed
-dy1 = 1 # player 1 y-speed
-score1 = 0 # player 1 score
-
-player2alive = True
-x2 = 150 # player 2 x-location
-y2 = 50  # player 2 y-location
-dx2 = 0  # player 2 x-speed
-dy2 = 1  # player 2 y-speed
-score2 = 0 # player 2 score
 
 AIalive = True
 xai = 100  # AI x-location
@@ -75,10 +61,10 @@ dxai = 0   # AI x speed
 dyai = 1   # AI y speed
 
 def printscores():
-    printscr("Player 1 keyboard controls: w, a, s, d",10,24,player1colour)
-    printscr("Score: "+str(score1),160,49,player1colour)
-    printscr("Player 2 keyboard controls: i, j, k, l",420,24,player2colour)
-    printscr("Score: "+str(score2),560,49,player2colour)
+    printscr("Player 1 keyboard controls: w, a, s, d",10,24,player1.colour)
+    printscr("Score: "+str(player1.score),160,49,player1.colour)
+    printscr("Player 2 keyboard controls: i, j, k, l",420,24,player2.colour)
+    printscr("Score: "+str(player2.score),560,49,player2.colour)
 
 printscores()
 
@@ -88,27 +74,19 @@ def startagain():
     global score1, score2
     if AIalive:
             printBIG("AI wins!!!",200,200,"yellow")
-    if player1alive:
-        score1 = score1 + 1
+    if player1.alive:
+        player1.score = player1.score + 1
         printBIG("Player 1 wins!!!",100,200,"yellow")
-    if player2alive:
-        score2 = score2 + 1
+    if player2.alive:
+        player2.score = player2.score + 1
         printBIG("Player 2 wins!!!",100,200,"yellow")
     canvastext.delete("all")
     printscores()
     canvastext.update()
     canvas1.update()
     time.sleep(2)
-    player1alive = True
-    x1 = 50 
-    y1 = 50 
-    dx1 = 0 
-    dy1 = 1
-    player2alive = True
-    x2 = 150 # player 2 x-location
-    y2 = 50  # player 2 y-location
-    dx2 = 0  # player 2 x-speed
-    dy2 = 1  # player 2 y-speed
+    player1.__init__(75,50,0,1,True,0,"#A0A0FF")
+    player2.__init__(160,50,0,1,True,0,"#FFA0A0")
     AIalive = True
     xai = 100  # AI x-location
     yai = 50   # AI y-location
@@ -133,31 +111,30 @@ def cleargrid():
 
 
 def mykey(event):
-    global dx1, dy1, dx2, dy2
     if event.char == "w":
-        dy1 = -1
-        dx1 = 0
+        player1.dx = 0
+        player1.dy = -1
     if event.char == "d":
-        dy1 = 0
-        dx1 = 1
+        player1.dx = 1
+        player1.dy = 0
     if event.char == "a":
-        dy1 = 0
-        dx1 = -1
+        player1.dx = -1
+        player1.dy = 0
     if event.char == "s":
-        dy1 = 1
-        dx1 = 0
+        player1.dx = 0
+        player1.dy = 1
     if event.char == "i":
-        dy2 = -1
-        dx2 = 0
+        player2.dx = 0
+        player2.dy = -1
     if event.char == "l":
-        dy2 = 0
-        dx2 = 1
+        player2.dx = 1
+        player2.dy = 0
     if event.char == "j":
-        dy2 = 0
-        dx2 = -1
+        player2.dx = -1
+        player2.dy = 0
     if event.char == "k":
-        dy2 = 1
-        dx2 = 0
+        player2.dx = 0
+        player2.dy = 1
 
 def explosion(x,y):
     drawdot(x,y, "black")
@@ -198,7 +175,7 @@ def goclearAI():
 
 def controlAI():
     if grid[xai+dxai][yai+dyai] == 1:
-        if random.randint(1,100) > 40 : # turn to avoid wall
+        if random.randint(1,100) > 10 : # turn to avoid wall
            goclearAI()
     elif random.randint(1,100) > 92: # make a random turn
         goclearAI()
@@ -225,34 +202,17 @@ def timerupdate():
     global x1,x2,y1,y2,xai,yai, player1alive, player2alive, AIalive
     controlAI()
     player1.move()
-    if player1alive:
-      x1 = x1 + dx1
-      y1 = y1 + dy1
-    if player2alive:
-      x2 = x2 + dx2
-      y2 = y2 + dy2
+    player2.move()
     if AIalive:
       xai = xai + dxai
       yai = yai + dyai
-    if grid[x1][y1] == 1:
-       if player1alive:
-            explosion(x1,y1) 
-       player1alive = False  
-    if grid[x2][y2] == 1:
-       if player2alive:
-            explosion(x2,y2) 
-       player2alive = False
     if grid[xai][yai] == 1:
        if AIalive:
             explosion(xai,yai)
        AIalive = False
-    if player1alive:
-       drawdot(x1,y1,player1colour)
-    if player2alive: 
-       drawdot(x2,y2,player2colour)
     if AIalive:  
        drawdot(xai,yai,AIcolour)
-    alivecount = sum([player1alive, player2alive, AIalive])
+    alivecount = sum([player1.alive, player2.alive, AIalive])
     if alivecount <= 1:
         startagain()
     mainwin.after(100,timerupdate)
